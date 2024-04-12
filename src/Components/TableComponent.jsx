6377a0e25708f6f63765e92d569f9f73ae1a7ace
Durvesh Pokade <durveshpokade835@gmail.com>
2024-04-12 13:59:19 +0530

    added Edit and Delete Functionality

--- src/Components/TableComponent.jsx
+++ src/Components/TableComponent.jsx
@@ -5,23 +5,18 @@ import axios from 'axios';
 
 export default function TableComponent() {
     const [records, setRecords] = useState([]);
-
+    const [editID, setEditID] = useState()
     const [inputData, setInputData] = useState({
         id: '',
         title: '',
         body: ''
     })
 
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
-            setRecords(res.data)
-        })
-    }, [])
 
     function handleSubmit(event) {
         event.preventDefault();
         axios.post('https://jsonplaceholder.typicode.com/posts', inputData).then(res => {
-            setRecords([res.data, ...records]);
+            setRecords([...records, res.data]);
             setInputData({
                 id: '',
                 title: '',
@@ -30,6 +25,40 @@ export default function TableComponent() {
             alert("Data Added succefully");
         })
     }
+    function handleDelete(deleteID) {
+        axios.delete(`https://jsonplaceholder.typicode.com/posts/${deleteID}`).then(res => {
+            const updatedRecords = [...records];
+            updatedRecords.splice(deleteID, 1);
+            setRecords(updatedRecords);
+        })
+    }
+
+    function handleEdit(eId) {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${eId}`).then(res => {
+            setInputData(res.data)
+
+        })
+    }
+    const handleUpdate = (event) => {
+        event.preventDefault();
+        axios.put(`https://jsonplaceholder.typicode.com/posts/${editID}`, inputData).then(res => {
+            // setRecords([res.data, ...records]);
+            setInputData({
+                id: '',
+                title: '',
+                body: ''
+            });
+            alert("Data Added succefully");
+        })
+    };
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+            setRecords(res.data)
+        })
+    }, [])
+
+
     return (<>
         <Container>
             <Form onSubmit={handleSubmit}>
@@ -54,7 +83,8 @@ export default function TableComponent() {
                             }} />
                     </Col>
                 </Row>
-                <Button variant="dark" type='submit'>submit</Button>
+                <Button variant="dark" type='submit'>Add</Button>
+                <Button variant="dark" type='submit' onClick={handleUpdate}>Update</Button>
             </Form>
         </Container>
         <Table striped bordered hover variant="dark">
@@ -63,6 +93,7 @@ export default function TableComponent() {
                     <th>id</th>
                     <th>title</th>
                     <th>Description</th>
+                    <th colSpan={2}>CRUD Operations</th>
                 </tr>
             </thead>
             <tbody>
@@ -71,6 +102,11 @@ export default function TableComponent() {
                         <td>{record.id}</td>
                         <td>{record.title}</td>
                         <td>{record.body}</td>
+                        <td><Button variant="light" type='submit' onClick={() => {
+                            handleEdit(record.id)
+                            setEditID(record.id)
+                        }}>Edit</Button></td>
+                        <td><Button variant="light" type='submit' onClick={() => handleDelete(id)}>Delete</Button></td>
                     </tr>
                 ))}
 
